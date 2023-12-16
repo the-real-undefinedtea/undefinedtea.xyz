b2ebf527189b3acd3316733bce6257e51e58b2c1
undefinedtea <hello@undefinedtea.dev>
2023-12-16 09:55:13 +0200

    chore: porvide astro 3, eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:astro/recommended',
+    'plugin:svelte/recommended',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    extraFileExtentions: ['*.svelte']
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    },
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  ],
+  rules: {
+  }
+}
